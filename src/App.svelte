ffd694ac744009181d4d5de2fa9392735c0c8998
ralex <ralex.rdz@gmail.com>
2019-10-01 01:17:31 -0500

    icons y icon componenet

---
+++ src/App.svelte
@@ -0,0 +1,15 @@
+<script>
+	import Icon from './components/icons/Icon.svelte'
+	import Icons from './components/icons/Icons.svelte'
+	export let name;
+</script>
+
+<style>
+	h1 {
+		color: purple;
+	}
+</style>
+
+<h1>Hello {name}!</h1>
+<Icons/>
+<Icon name="link"></Icon>
